38c00c606e71caf16fbf00102fb79a7c8babe288
studyantengh4cker <gwapolloyd124@gmail.com>
2025-03-18 03:58:10 +0800

    change image

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -23,7 +23,12 @@ export default function ChatPage() {
     <main className="h-screen max-h-screen flex">
       <section className="w-[30rem] bg-zinc-900 md:flex flex-col items-center justify-center gap-4 hidden">
         <div className="w-32 h-32 relative rounded-full overflow-hidden">
-          <Image src="/me3.jpg" fill alt="Hello" />
+          <Image
+            src="/me3.jpg"
+            fill
+            alt="Hello"
+            className="object-cover object-center"
+          />
         </div>
         <h2 className="text-2xl font-semibold text-white">Lloyd Semblante</h2>
       </section>
