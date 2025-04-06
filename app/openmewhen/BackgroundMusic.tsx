3ab0282c69cd2bbc95f6943f64779fab59dc1b78
studyantengh4cker <gwapolloyd124@gmail.com>
2025-04-06 20:35:40 +0800

    fk

--- app/openmewhen/BackgroundMusic.tsx
+++
@@ -1,8 +0,0 @@
-export function BackgroundMusic({ videoId }: { videoId: string }) {
-  return (
-    <iframe
-      className="w-full h-[30rem]"
-      src={`https://www.youtube.com/embed/${videoId}?loop=1`}
-    ></iframe>
-  );
-}
